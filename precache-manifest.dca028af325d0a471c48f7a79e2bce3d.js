1af310622c41bdc05497338f1f86c4adfb57d81b
Monica <monica.dixon8@gmail.com>
2020-01-26 20:59:12 -0600

    Updates

---
+++ precache-manifest.dca028af325d0a471c48f7a79e2bce3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e892bdbc194954502d3033efb255599",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dc5e75f3126ab8ee21a9",
+    "url": "/clicky-game/static/css/main.e8427268.chunk.css"
+  },
+  {
+    "revision": "6706450a8d3108003339",
+    "url": "/clicky-game/static/js/2.e6ce59e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.e6ce59e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc5e75f3126ab8ee21a9",
+    "url": "/clicky-game/static/js/main.ff7d0e2d.chunk.js"
+  },
+  {
+    "revision": "dce93861a1d9590743d1",
+    "url": "/clicky-game/static/js/runtime-main.0ac4616a.js"
+  }
+]);
